226779ba60fba5b4b370819978542a89a564936d
agnkos <101097155+agnkos@users.noreply.github.com>
2023-08-16 11:10:50 +0200

    exercises 2.2-2.3

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,10 +2,12 @@ import Header from "./Header";
 import Content from "./Content";
 
 const Course = ({ course }) => {
+    const sum = course.parts.reduce((acc, curr) => acc + curr.exercises, 0)
     return (
         <div>
             <Header name={course.name} />
             <Content parts={course.parts} />
+            <p>Total of {sum} exercises.</p>
         </div>
     )
 }
