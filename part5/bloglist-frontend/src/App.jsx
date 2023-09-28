4da88ae2be92307ee003adfeaee8d64a2528dfb2
agnkos <101097155+agnkos@users.noreply.github.com>
2023-09-28 10:41:20 +0200

    exercise 5.2 localStorage login, logout

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -15,12 +16,31 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleUsernameChange = ({ target }) => {
+    setUsername(target.value)
+  }
+  const handlePasswordChange = ({ target }) => {
+    setPassword(target.value)
+  }
+
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
 
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -30,63 +50,32 @@ const App = () => {
     }
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  )
+  const logOut = () => {
+    console.log('loggin out')
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
 
   if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    )
+    return <LoginForm
+      handleLogin={handleLogin}
+      username={username}
+      password={password}
+      handleUsernameChange={handleUsernameChange}
+      handlePasswordChange={handlePasswordChange}
+    />
   }
 
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <div className='flex'>
+        <p>
+          <span className='bolded'>{user.name} </span>
+          logged in
+        </p>
+        <button onClick={logOut}>Log out</button>
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
