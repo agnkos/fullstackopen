e49050daf89418217f0fcdd9785e46a3c7a7e89f
agnkos <101097155+agnkos@users.noreply.github.com>
2023-09-30 15:55:06 +0200

    exercise 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -134,8 +134,8 @@ const App = () => {
         />
       </Toggle>
       <div className='blogs-container'>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} addLike={addLike}/>
+        {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
+          <Blog key={blog.id} blog={blog} addLike={addLike} />
         )}
       </div>
     </div>
