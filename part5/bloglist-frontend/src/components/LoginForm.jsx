dadedfbd7705abc43a1ef1b05eb3f019d3a0c7f1
agnkos <101097155+agnkos@users.noreply.github.com>
2023-10-06 18:31:24 +0200

    exercise 5.17

--- part5/bloglist-frontend/src/components/LoginForm.jsx
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -1,41 +1,43 @@
 import PropTypes from 'prop-types'
 
 const LoginForm = ({ handleLogin, username, password, handleUsernameChange, handlePasswordChange }) => {
-    return (
-        <div>
-            <h2>Log in to application</h2>
-            <form onSubmit={handleLogin} className="form">
-                <div className="form-element">
-                    username
-                    <input
-                        type="text"
-                        value={username}
-                        name="Username"
-                        onChange={handleUsernameChange}
-                    />
-                </div>
-                <div className="form-element">
-                    password
-                    <input
-                        type="password"
-                        value={password}
-                        name="Password"
-                        onChange={handlePasswordChange}
-                    />
-                </div>
-                <button type="submit">login</button>
-            </form>
-
+  return (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin} className="form">
+        <div className="form-element">
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={handleUsernameChange}
+            id='username'
+          />
         </div>
-    )
+        <div className="form-element">
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={handlePasswordChange}
+            id='password'
+          />
+        </div>
+        <button type="submit" id='login-button'>log in</button>
+      </form>
+
+    </div>
+  )
 }
 
 LoginForm.propTypes = {
-    handleLogin: PropTypes.func.isRequired,
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired,
-    handleUsernameChange: PropTypes.func.isRequired,
-    handlePasswordChange: PropTypes.func.isRequired
+  handleLogin: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired
 }
 
 export default LoginForm
