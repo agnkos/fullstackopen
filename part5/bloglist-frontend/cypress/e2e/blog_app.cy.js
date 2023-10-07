3dee1a1e0072f412b010b7ecfe978c8513dda159
agnkos <101097155+agnkos@users.noreply.github.com>
2023-10-07 13:16:50 +0200

    exercise 5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Agnieszka Kosiedowska',
+      username: 'aga',
+      password: 'Piotrek123'
+    }
+    cy.request('POST', 'http://localhost:5173/api/users', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -8,5 +14,27 @@ describe('Blog app', function () {
     cy.get('#username')
     cy.get('#password')
     cy.get('#login-button')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('aga')
+      cy.get('#password').type('Piotrek123')
+      cy.get('#login-button').click()
+
+      cy.get('html').should('contain', 'Agnieszka Kosiedowska logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('Piotrek')
+      cy.get('#password').type('aga123')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-color', 'rgb(255, 0, 0)')
+    })
+
   })
 })
--- part5/bloglist-frontend/src/components/Notification.jsx
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -2,16 +2,16 @@ import PropTypes from 'prop-types'
 
 const Notification = ({ message, errorMessage }) => {
 
-    return (
-        <div className={`${message ? 'message' : errorMessage ? 'error' : ''}`}>
-            {message || errorMessage}
-        </div>
-    )
+  return (
+    <div className={`${message ? 'message' : errorMessage ? 'error' : ''}`}>
+      {message || errorMessage}
+    </div>
+  )
 }
 
 Notification.propTypes = {
-    message: PropTypes.string,
-    errorMessage: PropTypes.string
+  message: PropTypes.string,
+  errorMessage: PropTypes.string
 }
 
 export default Notification
