4e7377a45031e43adf79a81fda46919786baf3b4
agnkos <101097155+agnkos@users.noreply.github.com>
2023-10-07 15:15:11 +0200

    exercise 5.21

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -55,20 +55,45 @@ describe('Blog app', function () {
       cy.get('.blogs-container').contains('www.frontend.blogspot.com')
     })
 
-    it.only('user can like a blog', function () {
-      cy.contains('add blog').click()
-      // cy.get('.title-input').type('frontend blog')
-      // cy.get('.author-input').type('aga kos')
-      // cy.get('.url-input').type('www.frontend.blogspot.com')
-      // cy.get('.add-btn').click()
+    describe('several blogs exis', function () {
+      beforeEach(function () {
+        cy.createBlog({ author: 'aga kos', title: 'frontend blog', url: 'www.frontend.blogspot.com' })
+        cy.createBlog({ author: 'aga', title: 'bike blog', url: 'www.mybike.com' })
+      })
 
-      cy.createBlog({ author: 'aga kos', title: 'frontend blog', url: 'www.frontend.blogspot.com' })
-      cy.createBlog({ author: 'aga', title: 'bike blog', url: 'www.mybike.com' })
+      it('user can like a blog', function () {
+        cy.contains('frontend blog').parent().find('.show-btn').click()
+        cy.contains('frontend blog').parent().find('.like-btn').click()
 
-      cy.contains('frontend blog').parent().find('.show-btn').click()
-      cy.contains('frontend blog').parent().find('.like-btn').click()
-      cy.get('.blog-likes').should('include.text', 'likes: 1')
+        cy.get('.blog-likes').should('include.text', 'likes: 1')
+      })
 
+      it('a user who created a blog can delete it', function () {
+        cy.contains('bike blog').parent().find('.show-btn').click()
+        cy.contains('bike blog').parent().find('.delete-btn').click()
+
+        cy.get('.blogs-container').should('not.contain', 'bike blog')
+        cy.get('.message').should('contain', 'Blog deleted')
+      })
+
+      it('a user who has not created a blog cannot delete it', function () {
+        cy.contains('Log out').click()
+
+        const user = {
+          name: 'Piotr K',
+          username: 'piotr',
+          password: 'Aga123'
+        }
+        cy.request('POST', 'http://localhost:5173/api/users', user)
+        cy.login({ username: 'piotr', password: 'Aga123' })
+        cy.contains('bike blog').parent().find('.show-btn').click()
+        cy.contains('bike blog').parent().find('.delete-btn').click()
+
+        cy.get('.blogs-container').should('contain', 'bike blog')
+        cy.get('.error').should('contain', 'Request failed with status code 401')
+      })
     })
+
+
   })
 })
--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -33,15 +33,14 @@ Cypress.Commands.add('login', ({ username, password }) => {
   })
 })
 
-Cypress.Commands.add('createBlog', ({ title, author, url }) => {
+Cypress.Commands.add('createBlog', ({ title, author, url, user }) => {
   cy.request({
     url: 'http://localhost:3003/api/blogs',
     method: 'POST',
-    body: { title, author, url },
+    body: { title, author, url, user },
     headers: {
       'Authorization': `Bearer ${JSON.parse(localStorage.getItem('loggedBlogappUser')).token}`
     }
   })
-
   cy.visit('http://localhost:5173')
 })
