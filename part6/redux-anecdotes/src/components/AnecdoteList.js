2a1a8e3ed5d7e607f3a528c5dac7cca0dcaea197
agnkos <101097155+agnkos@users.noreply.github.com>
2023-10-11 10:33:18 +0200

    exercise 6.10

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,9 +4,8 @@ import { addVote } from "../reducers/anecdoteReducer"
 const AnecdoteList = () => {
 
     const dispatch = useDispatch()
-    // const anecdotes = useSelector(state => state.anecdotes)
 
-    const anecdotes = useSelector(state => {
+    const anecdotes = useSelector((state) => {
         if (state.filter !== '') {
             return state.anecdotes.filter(a => a.content.toLowerCase().includes(state.filter.toLowerCase()))
         } else return state.anecdotes
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,17 @@
-export const filterChange = filter => {
-    return {
-        type: 'SET_FILTER',
-        payload: filter,
-    }
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-const filterReducer = (state = '', action) => {
-    switch (action.type) {
-        case 'SET_FILTER':
-            return action.payload
-        default:
-            return state
-    }
-}
+const initialState = ''
 
-export default filterReducer
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+        filterChange(state, action) {
+            return state = action.payload
+        }
+    }
+})
+
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
+
