e72a782cfa1a9dbebef950ef458a862062e19ab7
agnkos <101097155+agnkos@users.noreply.github.com>
2023-11-28 14:48:29 +0100

    exercise 8.6

--- part8/graphQL-proj/index.js
+++ part8/graphQL-proj/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
     {
@@ -125,6 +126,15 @@ const typeDefs = `
    allBooks(author: String, genre: String): [Book]
    allAuthors: [Author]
   }
+
+  type Mutation {
+    addBook(
+        title: String
+        author: String
+        published: Int
+        genres: [String]
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -132,7 +142,7 @@ const resolvers = {
         bookCount: () => books.length,
         authorCount: () => authors.length,
         allBooks: (root, args) => {
-            if (!args) {
+            if (!args.author && !args.genre) {
                 return books
             }
             if (args.author && !args.genre) {
@@ -150,6 +160,17 @@ const resolvers = {
     Author: {
         bookCount: (root) => {
             return books.filter(book => book.author === root.name).length
+        }
+    },
+    Mutation: {
+        addBook: (root, args) => {
+            const book = { ...args, id: uuid() }
+            books = books.concat(book)
+            if (!authors.find(author => author.name === args.author)) {
+                const author = { name: args.author }
+                authors = authors.concat(author)
+            }
+            return book
         }
     }
 
